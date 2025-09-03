587331cb2fc0f27160bd37cc9db2863514719104
lorenzosalti <lorenzosalti@gmail.com>
2025-09-03 17:56:55 +0200

    chore: add custom query findByName in pizza repository

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repositories/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repositories/PizzaRepository.java
@@ -1,8 +1,13 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.repositories;
 
+import java.util.List;
+
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
+  // CUSTOM QUERYS
+
+  public List<Pizza> findByName(String name);
 }
