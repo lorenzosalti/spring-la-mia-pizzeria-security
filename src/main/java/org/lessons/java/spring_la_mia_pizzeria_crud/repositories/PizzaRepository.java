e6a396b67d696074e1a3a69f56ba3bddc697e3d8
lorenzosalti <lorenzosalti@gmail.com>
2025-09-03 14:19:09 +0200

    chore: add PizzaRepository as JPA repository

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repositories/PizzaRepository.java
@@ -0,0 +1,8 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.repositories;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+}
