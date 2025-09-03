e6403c7a086a40863e595a4d48faf0a387844fde
lorenzosalti <lorenzosalti@gmail.com>
2025-09-03 14:04:16 +0200

    feat: created controller for homepage

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+  @GetMapping
+  public String home() {
+    return "home";
+  }
+}
