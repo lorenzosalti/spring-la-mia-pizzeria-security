7fcae4dbabdde3b2e759f387e59eb68440feee48
lorenzosalti <lorenzosalti@gmail.com>
2025-09-03 14:27:35 +0200

    feat: wired repository and add pizzas list to model

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -1,6 +1,12 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
 
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,8 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/pizzas")
 public class PizzaController {
 
+  @Autowired
+  private PizzaRepository repository;
+
   @GetMapping
-  public String index() {
+  public String index(Model model) {
+    List<Pizza> pizzas = repository.findAll();
+    model.addAttribute("pizzas", pizzas);
     return "pizzas/index";
   }
 
