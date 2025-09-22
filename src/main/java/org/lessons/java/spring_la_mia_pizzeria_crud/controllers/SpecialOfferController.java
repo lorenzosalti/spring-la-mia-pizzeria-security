63ba28aa7d28148c3eef1dee81f6b938fc975b0f
lorenzosalti <lorenzosalti@gmail.com>
2025-09-22 17:51:01 +0200

    chore: SpecialOfferRepository injection in SpecialOfferController

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/SpecialOfferController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/SpecialOfferController.java
@@ -1,6 +1,8 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.SpecialOffer;
+import org.lessons.java.spring_la_mia_pizzeria_crud.repositories.SpecialOfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +15,9 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/pizzas")
 public class SpecialOfferController {
+
+  @Autowired
+  private SpecialOfferRepository offerRepository;
 
   @PostMapping("/{id}/offer")
   public String storeOffer(@Valid @ModelAttribute("offer") SpecialOffer offerToStore, BindingResult bindingResult,
