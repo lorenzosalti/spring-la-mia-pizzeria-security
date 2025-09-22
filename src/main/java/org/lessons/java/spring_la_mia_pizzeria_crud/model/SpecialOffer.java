e72f86cb15da6384927bc1dde8f45347f3d46ac1
lorenzosalti <lorenzosalti@gmail.com>
2025-09-22 15:53:11 +0200

    chore: created SpecialOffer model

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/SpecialOffer.java
@@ -0,0 +1,59 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "offers")
+public class SpecialOffer {
+
+  // ATTRIBUTES
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(name = "starting_date", nullable = false)
+  @NotNull(message = "La data di inizio è necessaria")
+  private LocalDate startingDate;
+
+  @Column(name = "ending_date", nullable = false)
+  @NotNull(message = "La data di fine è necessaria")
+  private LocalDate endingDate;
+
+  // CONSTRUCTOR
+  public SpecialOffer() {
+  }
+
+  // GETTERS AND SETTERS
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public LocalDate getStartingDate() {
+    return startingDate;
+  }
+
+  public void setStartingDate(LocalDate startingDate) {
+    this.startingDate = startingDate;
+  }
+
+  public LocalDate getEndingDate() {
+    return endingDate;
+  }
+
+  public void setEndingDate(LocalDate endingDate) {
+    this.endingDate = endingDate;
+  }
+
+}
