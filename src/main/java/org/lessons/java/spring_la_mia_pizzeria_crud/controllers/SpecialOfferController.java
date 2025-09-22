3c48aad8c270e8debf26129c95b6036e88c77d83
lorenzosalti <lorenzosalti@gmail.com>
2025-09-22 17:38:44 +0200

    chore: create SpecialOfferController with storeOffer method

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/SpecialOfferController.java
@@ -0,0 +1,28 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.SpecialOffer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/pizzas")
+public class SpecialOfferController {
+
+  @PostMapping("/{id}/offer")
+  public String storeOffer(@Valid @ModelAttribute("offer") SpecialOffer offerToStore, BindingResult bindingResult,
+      Model model) {
+
+    if (bindingResult.hasErrors()) {
+      return "$(err)";
+    }
+
+    return "";
+  }
+
+}
