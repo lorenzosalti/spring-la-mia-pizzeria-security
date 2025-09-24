f9f1ebee7eb0c63c14529c12521189897d529118
lorenzosalti <lorenzosalti@gmail.com>
2025-09-24 18:36:58 +0200

    chore: add edit paraameter in edit method

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -55,13 +55,13 @@ public class PizzaController {
   @GetMapping("/create")
   public String create(Model model) {
     model.addAttribute("pizza", new Pizza());
-    return "/pizzas/create";
+    return "/pizzas/create-edit";
   }
 
   @PostMapping("/create")
   public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
     if (bindingResult.hasErrors()) {
-      return "/pizzas/create";
+      return "/pizzas/create-edit";
     }
     pizzaRepository.save(formPizza);
     return "redirect:/pizzas";
@@ -71,13 +71,14 @@ public class PizzaController {
   @GetMapping("/edit/{id}")
   public String edit(@PathVariable("id") Integer id, Model model) {
     model.addAttribute("pizza", pizzaRepository.findById(id).get());
-    return "/pizzas/edit";
+    model.addAttribute("edit", true);
+    return "/pizzas/create-edit";
   }
 
   @PostMapping("/edit/{id}")
   public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
     if (bindingResult.hasErrors()) {
-      return "/pizzas/edit";
+      return "/pizzas/create-edit";
     }
     pizzaRepository.save(formPizza);
     return "redirect:/pizzas";
