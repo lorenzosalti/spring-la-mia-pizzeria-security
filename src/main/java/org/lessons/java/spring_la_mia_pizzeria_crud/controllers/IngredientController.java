dd894ecc1836aa8db2fcdc5e9e582d1e5d47b391
lorenzosalti <lorenzosalti@gmail.com>
2025-09-24 17:18:44 +0200

    chore:  add create and store methods

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/IngredientController.java
@@ -7,9 +7,14 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repositories.IngredientRepos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -29,5 +34,22 @@ public class IngredientController {
   public String show(@PathVariable Integer id, Model model) {
     model.addAttribute("ingredient", ingredientRepository.findById(id).get());
     return "ingredients/show";
+  }
+
+  @GetMapping("/create")
+  public String create(Model model) {
+    Ingredient ingredient = new Ingredient();
+    model.addAttribute("ingredient", ingredient);
+    return "ingredients/create-edit";
+  }
+
+  @PostMapping("/create")
+  public String store(@Valid @ModelAttribute("ingredient") Ingredient ingredientToStore, BindingResult bindingResult,
+      Model model) {
+    if (bindingResult.hasErrors()) {
+      return "ingredients/create-edit";
+    }
+    ingredientRepository.save(ingredientToStore);
+    return "redirect:/ingredients";
   }
 }
