575b1421e37ade3f19574a3afecd275644660a5b
lorenzosalti <lorenzosalti@gmail.com>
2025-09-27 16:35:28 +0200

    chore: add utility methods

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/service/PizzaService.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/service/PizzaService.java
@@ -1,5 +1,9 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.service;
 
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Ingredient;
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +13,34 @@ public class PizzaService {
 
   @Autowired
   private PizzaRepository pizzaRepository;
+
+  public List<Pizza> findAll() {
+    return pizzaRepository.findAll();
+  }
+
+  public List<Pizza> findByName(String name) {
+    return pizzaRepository.findByNameContaining(name);
+  }
+
+  public Pizza getById(Integer id) {
+    return pizzaRepository.findById(id).get();
+  }
+
+  public Pizza create(Pizza pizza) {
+    return pizzaRepository.save(pizza);
+  }
+
+  public Pizza update(Pizza pizza) {
+    return pizzaRepository.save(pizza);
+  }
+
+  public void deleteById(Integer id) {
+    Pizza pizzaToDelete = getById(id);
+    for (Ingredient ingredient : pizzaToDelete.getIngredients()) {
+      List<Pizza> pizzas = ingredient.getPizzas();
+      pizzas.remove(pizzaToDelete);
+    }
+    pizzaRepository.deleteById(id);
+  }
 
 }
