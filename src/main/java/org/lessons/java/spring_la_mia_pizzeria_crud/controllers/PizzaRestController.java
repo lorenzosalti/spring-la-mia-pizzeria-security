5c93fdd05fbccc2e16dbfa568c1b074bd0fa266d
lorenzosalti <lorenzosalti@gmail.com>
2025-09-27 17:54:55 +0200

    feat: add show

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 // import org.lessons.java.spring_la_mia_pizzeria_crud.service.IngredientService;
@@ -10,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +30,20 @@ public class PizzaRestController {
   @GetMapping
   public ResponseEntity<List<Pizza>> index() {
     return new ResponseEntity<List<Pizza>>(pizzaService.findAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/search")
+  public ResponseEntity<List<Pizza>> searchByName(@RequestParam(name = "name") String name) {
+    return new ResponseEntity<List<Pizza>>(pizzaService.findByName(name),
+        HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Pizza> show(@PathVariable Integer id) {
+    Optional<Pizza> pizzaAttempt = pizzaService.findById(id);
+    if (pizzaAttempt.isEmpty()) {
+      return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<Pizza>(pizzaAttempt.get(), HttpStatus.OK);
   }
 }
