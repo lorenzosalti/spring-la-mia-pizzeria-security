25074ed691cab40f39a4e82fbe862602f2da6270
lorenzosalti <lorenzosalti@gmail.com>
2025-09-28 19:01:57 +0200

    feat: add store method

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaRestController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +47,10 @@ public class PizzaRestController {
       return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
     }
     return new ResponseEntity<Pizza>(pizzaAttempt.get(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Pizza> store(@RequestBody Pizza pizzaToStore) {
+    return new ResponseEntity<Pizza>(pizzaService.create(pizzaToStore), HttpStatus.OK);
   }
 }
