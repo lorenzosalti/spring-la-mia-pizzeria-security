a10f9a530c5bc4091c174be934f9066372093bce
lorenzosalti <lorenzosalti@gmail.com>
2025-09-30 10:45:28 +0200

    chore: add entities User and Role

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Role.java
@@ -0,0 +1,57 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NotBlank
+  private String name;
+
+  @ManyToMany(mappedBy = "roles")
+  @JsonBackReference
+  private Set<User> users;
+
+  public Role() {
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<User> getUsers() {
+    return users;
+  }
+
+  public void setUsers(Set<User> users) {
+    this.users = users;
+  };
+
+}
