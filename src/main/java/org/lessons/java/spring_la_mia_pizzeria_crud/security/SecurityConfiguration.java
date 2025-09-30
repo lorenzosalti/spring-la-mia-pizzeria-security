bf354fa596120d6ab3da73c58008d76224f18e5d
lorenzosalti <lorenzosalti@gmail.com>
2025-09-30 16:24:39 +0200

    feat: add filter chain requests

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.security;
 
+import org.springframework.security.config.Customizer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +18,17 @@ public class SecurityConfiguration {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+    http.authorizeHttpRequests(requests -> requests
+        .requestMatchers("/pizzas/create", "/pizzas/edit/**").hasAuthority("ADMIN")
+        .requestMatchers(HttpMethod.POST, "/pizzas/**").hasAuthority("ADMIN")
+        .requestMatchers("/offers/create", "/offers/edit/**").hasAuthority("ADMIN")
+        .requestMatchers(HttpMethod.POST, "/offers/**").hasAuthority("ADMIN")
+        .requestMatchers("/ingredients", "/ingredients/**").hasAuthority("ADMIN")
+        .requestMatchers("/pizzas", "/pizzas/**", "/offeers/**").hasAnyAuthority("USER", "ADMIN")
+        .requestMatchers("/", "/**").permitAll())
+        .formLogin(Customizer.withDefaults())
+        .cors(cors -> cors.disable())
+        .csrf(csrf -> csrf.disable());
     return http.build();
   }
 
